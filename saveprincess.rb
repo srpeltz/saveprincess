50785f2ee39659c93dab785c979487edd83d425b
steven peltzer <srpeltzer@yahoo.com>
2020-01-10 18:18:58 -0800

    both challenges completed

---
+++ saveprincess.rb
@@ -0,0 +1,54 @@
+require "minitest/autorun"
+
+def displayPathtoPrincess(n,grid)
+    princessCoordinates = Hash.new
+    botCoordinates = Hash.new
+    moves = ""
+
+    grid.each_with_index do |el, i| #find player coordinates
+        foundP = el.split(//).find_index("p")
+        foundM = el.split(//).find_index("m")
+
+        if foundP #found princess
+            princessCoordinates[:x] = el.split(//).find_index("p")
+            princessCoordinates[:y] = i
+        elsif foundM #found bot
+            botCoordinates[:x] = el.split(//).find_index("m")
+            botCoordinates[:y] = i
+        end
+
+    end #grid.each_with_index
+
+    if !princessCoordinates.empty? && !botCoordinates.empty? #if empty then no 'm' or 'p' found in grid, therefore no moves
+
+        while princessCoordinates != botCoordinates do #if hashes are not the same then 'm' has not found 'p'
+
+            ycoordDistance = princessCoordinates[:y] <=> botCoordinates[:y] #check difference in y coordinates
+            if ycoordDistance != 0
+                botCoordinates[:y] = botCoordinates[:y] + ycoordDistance
+                moves += ycoordDistance.negative? ? "UP\n" : "DOWN\n"
+            end
+
+            xcoordDistance = princessCoordinates[:x] <=> botCoordinates[:x] #check difference in x coordinates
+            if xcoordDistance != 0
+                botCoordinates[:x] = botCoordinates[:x] + xcoordDistance
+                moves += xcoordDistance.negative? ? "LEFT\n" : "RIGHT\n"
+            end
+
+        end #while princessCoordinates != botCoordinates
+
+    end #if coordinate hashes not empty?
+
+    print moves
+end #displayPathtoPrincess
+
+
+m = gets.to_i
+
+grid = Array.new(m)
+
+(0...m).each do |i|
+    grid[i] = gets.strip
+end
+
+displayPathtoPrincess(m,grid)
